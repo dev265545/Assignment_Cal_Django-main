b24a3a6981a47944a1d40fac581fd3bc86afed36
Dev Gupta <guptadev265@gmail.com>
2022-07-07 03:09:31 +0530

    commit

--- main/views.py
+++ main/views.py
@@ -4,5 +4,3 @@ from django.http import HttpResponse, HttpResponseBadRequest
 # Create your views here.
 def index(response):
     return render(response,"main/base.html",{})
-def v1(response):
-    return HttpResponse("<h1>Calendar app 2</h1>")
